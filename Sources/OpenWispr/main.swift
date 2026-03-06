9268597adb6d28c03c77eb4adf4350dbf0c49938
Ammon Taylor <ammonx9@gmail.com>
2026-03-06 06:50:03 -0700

    Bump version to 0.13.0

--- Sources/OpenWispr/main.swift
+++ Sources/OpenWispr/main.swift
@@ -5,7 +5,7 @@ setvbuf(stdout, nil, _IOLBF, 0)
 setvbuf(stderr, nil, _IOLBF, 0)
 
 enum OpenWispr {
-    static let version = "0.12.0"
+    static let version = "0.13.0"
 }
 
 let version = OpenWispr.version
